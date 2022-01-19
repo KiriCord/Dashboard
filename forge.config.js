5746f1bbbab59bbb744c7247bab754a53ef9ee49
kiricord <rikudosennin410@gmail.com>
2022-01-19 10:34:38 +0500

    add forge.config.js

---
+++ forge.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const rootDir = process.cwd();
+
+module.exports = {
+    packagerConfig: {
+        asar: true,
+    },
+
+    makers: [
+        {
+            "name": "@electron-forge/maker-squirrel",
+            "config": {
+                "name": "dashboard"
+            },
+        },
+        {
+            "name": "@electron-forge/maker-zip",
+            "platforms": [
+              "darwin"
+            ]
+        },
+        {
+            "name": "@electron-forge/maker-deb",
+            "config": {}
+        },
+        {
+            "name": "@electron-forge/maker-rpm",
+            "config": {}
+        },
+    ],
+    plugins: [
+        [
+            '@electron-forge/plugin-webpack',
+            {
+                devContentSecurity: '',
+                port: 3000,
+                loggerPort: 9000,
+                mainConfig: path.join(rootDir, 'webpack/webpack.main.config.js'),
+                renderer: {
+                    config: path.join(rootDir, 'webpack/webpack.renderer.config.js'),
+                    entryPoints: [
+                        {
+                            name: 'main_window',
+                            rhmr: 'react-hot-loader/patch',
+                            html: path.join(rootDir, 'public/index.html'),
+                            js: path.join(rootDir, 'src/renderer/renderer.tsx'),
+                            preload: {
+                                js: path.join(rootDir, 'src/renderer/rendererPreload.tsx')
+                            },
+                        },
+                    ],
+                },
+            },
+        ],
+    ],
+};
