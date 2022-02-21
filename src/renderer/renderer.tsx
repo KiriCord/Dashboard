42835f511ad29a864fe9ccb30b46b376704b6f59
kiricord <rikudosennin410@gmail.com>
2022-02-21 15:39:25 +0500

    Removed unnecessary

--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -1,14 +1,6 @@
-//import './components/AppChart';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Layout from './components/Layout';
-import App from './components/Mainbar';
-//import Sidebar from './components/sidebar/sidebar';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
-import Page1 from './pages/Page1';
-import Mainbar from './components/Mainbar';
-
 
 window.api.messageApi.receive("fromMain", (message: any) => {
   console.log(message);
@@ -17,13 +9,7 @@ window.api.messageApi.receive("fromMain", (message: any) => {
 window.api.messageApi.send("toMain", "Hello World");
 
 const app = (
-  //<Layout />
-  <BrowserRouter>
-    <Routes>
-      <Route path="/main_window" element={<Mainbar />} />
-      <Route path="/page-1" element={<Page1 />} />
-    </Routes>
-  </BrowserRouter>
+  <Layout />
 );
 
 ReactDOM.render(app, document.getElementById("core"));
