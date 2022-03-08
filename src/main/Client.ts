483ae1795c26ac1d3445c06ac4977cda432bd32c
kiricord <rikudosennin410@gmail.com>
2022-03-08 20:31:37 +0500

    refactor client.ts

--- src/main/Client.ts
+++ src/main/Client.ts
@@ -3,35 +3,54 @@ import { ICommunication } from "./Interface/ICommunication";
 
 export class Client implements ICommunication {
 
-     URL: string;
+  URL: string;
+  Socket: WebSocket;
 
-    constructor(wsURL: string) {
-      this.URL = wsURL;
+  constructor(wsURL: string) {
+    this.URL = wsURL;
+    this.Socket = new WebSocket(this.URL);
+
+    this.Socket.on('open', () => {
+      console.log("[Open] Соединение установлено");
+    });
+
+    this.Socket.on('close', (e) => {
+      console.log('[Close] Соединение прервано');
+    });
+
+    this.Socket.on('error', (error) => {
+      console.log(`[Error] ${error.message}`);
+    });
+  }
+
+  public onMessage(listener: (this: WebSocket, data: WebSocket.RawData, isBinary: boolean) => void) {
+    this.Socket.on('message', listener);
+  }
+
+  /*public Mess() {
+
+    socket.onopen = function (e) {
+      console.log("[Open] Соединение установлено");
+      //socket.send());
     }
-
-    public startWebSocket() {
-      var socket = new WebSocket(this.URL);
-
-        socket.onopen = function(e) {
-          console.log("[Open] Соединение установлено");
-          //socket.send());
+  
+    socket.onmessage = function (event) {  //TODO: Определение типа сообщений. 
+      let message = JSON.parse(event.data as string);
+      console.log('[Server]', message);
+    };
+  
+    socket.onclose = function (event) {
+      if (event.wasClean) {
+        console.log(`[Close] Соединение закрыто чисто, код=${event.code} причина=${event.reason}`);
+      } else {
+        console.log('[Close] Соединение прервано');
+        console.log('[Close] Попытка переподключения...');
+        setTimeout(startWebSocket(), 5000);
       }
-
-      socket.onmessage = function(event) {
-        let message = JSON.parse(event.data as string);
-        console.log('[Server]', message);
-      };
-      
-      socket.onclose = function(event) {
-        if (event.wasClean) {
-            console.log(`[Close] Соединение закрыто чисто, код=${event.code} причина=${event.reason}`);
-        } else {
-          console.log('[Close] Соединение прервано');
-        }
-      };
-      
-      socket.onerror = function(error) {
-        console.log(`[Error] ${error.message}`);
-      };
-    } 
+    };
+  
+    socket.onerror = function (error) {
+      console.log(`[Error] ${error.message}`);
+    };
+  }*/
 }
