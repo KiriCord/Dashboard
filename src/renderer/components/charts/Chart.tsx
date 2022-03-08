7198d7fc9e0e946617b1e6bdcc259d323706a47b
kiricord <rikudosennin410@gmail.com>
2022-03-08 20:32:55 +0500

    fill

--- src/renderer/components/charts/Chart.tsx
+++ src/renderer/components/charts/Chart.tsx
@@ -29,7 +29,7 @@ export const data = {
   datasets: [{
     label: 'My First Dataset',
     data: [65, 59, 80, 81, 56, 55, 40],
-    fill: false,
+    fill: true,
     borderColor: 'rgb(75, 192, 192)',
     tension: 0.1
   }]
