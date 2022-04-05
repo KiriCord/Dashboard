5138bd89556c33127c5e83ad83b6c4950600c2d7
kiricord <rikudosennin410@gmail.com>
2022-04-05 18:36:38 +0500

    add status server

---
+++ src/renderer/components/navbar/StatusServer.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Badge from '@mui/material/Badge';
+import { styled } from '@mui/material/styles';
+
+/* 
+    backgroundColor: '#44b700', //active server color
+        color: '#44b700',
+
+*/
+
+const StatusServer = styled(Badge)(({ theme }) => ({
+    '& .MuiBadge-badge': {
+        backgroundColor: '#b80000',
+        color: '#b80000',
+        boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+        '&::after': {
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            borderRadius: '50%',
+            animation: 'ripple 1.2s infinite ease-in-out',
+            border: '1px solid currentColor',
+            content: '""',
+        },
+    },
+    '@keyframes ripple': {
+        '0%': {
+            transform: 'scale(.8)',
+            opacity: 1,
+        },
+        '100%': {
+            transform: 'scale(2.4)',
+            opacity: 0,
+        },
+    },
+}));
+
+export default StatusServer;
