f532fe21b146bc4fb0fd16fb05bc52a76b38a933
kiricord <rikudosennin410@gmail.com>
2022-05-10 23:02:14 +0500

    add test char

--- src/renderer/components/charts/Test.tsx
+++ src/renderer/components/charts/Test.tsx
@@ -1,40 +1,49 @@
 import React from "react";
 import { useTheme } from '@mui/material/styles';
-import { LineChart, Line, XAxis, YAxis, ResponsiveContainer, Tooltip, Legend, CartesianGrid } from 'recharts';
+import { AreaChart, Area, Line, XAxis, YAxis, ResponsiveContainer, Tooltip, Legend, CartesianGrid } from 'recharts';
+import { ChartProps, Mer } from "../../types";
 
-function createData(time: string, amount?: number) {
-    return { time, amount };
+
+function createData(labels: string[], oil: Number[]) {
+    let res = labels.map((item, index) => {
+        return { 'Дата': item, 'Нефть': oil[index] }
+    });
+    return res;
 }
 
-const data = [
-    createData('00:00', 0),
-    createData('03:00', 300),
-    createData('06:00', 600),
-    createData('09:00', 800),
-    createData('12:00', 1500),
-    createData('15:00', 2000),
-    createData('18:00', 2400),
-    createData('21:00', 2400),
-    createData('24:00', 2600),
-];
+export const TestChart = (props: ChartProps) => {
+    const toDate = (mer: Mer) => {
+        const date = new Date(mer["dt"] as string);
+        const month = date.getMonth() + 1;
+        return `${month >= 10 ? month : "0" + month.toString()}/${date.getFullYear()}`
+    }
 
-export const TestChart = () => {
+    const labels = props.data.map(toDate);
+    const gas = props.data.map(item => item["gas"]);
+    const oil = props.data.map(item => item["oil"]);
+
     const theme = useTheme();
     return (
         <>
             <ResponsiveContainer>
-                <LineChart
-                    data={data}
+                <AreaChart
+                    data={createData(labels, oil)}
                     margin={{
-                        top: 16,
-                        right: 16,
+                        top: 0,
+                        right: 25,
                         bottom: 0,
                         left: 0,
                     }}
                 >
+                    <defs>
+                        <linearGradient id="colorOil" x1="0" y1="0" x2="0" y2="1">
+                            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                            <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+                        </linearGradient>
+                    </defs>
                     <CartesianGrid strokeDasharray="3 3" />
                     <XAxis
-                        dataKey="time"
+                        dataKey="Дата"
                         stroke={theme.palette.text.secondary}
                         style={theme.typography.body2}
                     />
@@ -43,16 +52,17 @@ export const TestChart = () => {
                         style={theme.typography.body2}
                     >
                     </YAxis>
-                    <Line
+                    <Area type="monotone" dataKey="Нефть" stroke="#8884d8" fillOpacity={1} fill="url(#colorOil)" />
+                    {/* <Line
                         isAnimationActive={false}
                         type="monotone"
-                        dataKey="amount"
+                        dataKey="oil"
                         stroke={theme.palette.primary.main}
                         dot={false}
-                    />
+                    /> */}
                     <Tooltip />
-                    {/* <Legend /> */}
-                </LineChart>
+                    <Legend />
+                </AreaChart>
             </ResponsiveContainer>
         </>
     );
