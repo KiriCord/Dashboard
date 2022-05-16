1558e4cafbba72e4d6d62a4b38711b48419d8969
kiricord <rikudosennin410@gmail.com>
2022-05-17 00:40:08 +0500

    fix

--- src/renderer/components/charts/Production/OilLiqGasPie.tsx
+++ src/renderer/components/charts/Production/OilLiqGasPie.tsx
@@ -26,14 +26,12 @@ export const OilLiqGasPie = (props: MerProps) => {
                 <PieChart width={400} height={400}>
                     <Pie
                         data={data}
-                        isAnimationActive={true}
                         cx="50%"
                         cy="50%"
-                        labelLine={true}
+                        labelLine={false}
                         outerRadius={150}
                         fill="#8884d8"
                         dataKey="value"
-                        label
                     >
                         {data.map((entry, index) => (
                             <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
