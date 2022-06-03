520e9f50a8d7ca56a891632b647ea1cfa1e3d0fc
kiricord <rikudosennin410@gmail.com>
2022-06-03 20:30:37 +0500

    fix bug render and add label

--- src/renderer/components/charts/Production/OilLiqGasPie.tsx
+++ src/renderer/components/charts/Production/OilLiqGasPie.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { useTheme } from '@mui/material/styles';
 import { PieChart, Pie, Legend, Cell, ResponsiveContainer, Tooltip } from 'recharts';
 import { MerProps, Mer } from "../../../types";
@@ -7,33 +7,47 @@ import { Box, Typography } from "@mui/material";
 
 const COLORS = ['#82ca9d', '#8884d8', '#82A6CA'];
 
+type dataGOL = {
+    name: string;
+    value: number;
+}
+
 export const OilLiqGasPie = (props: MerProps) => {
-    const gas = props.dataMer.map(item => Number.parseInt(item["gas"].toFixed())).reduce((a, b) => a + b, 0);
-    const oil = props.dataMer.map(item => Number.parseInt(item["oil"].toFixed())).reduce((a, b) => a + b, 0);
-    const liq = props.dataMer.map(item => Number.parseInt(item["liq"].toFixed())).reduce((a, b) => a + b, 0);
-    const data = [
-        { name: 'Газ', value: gas },
-        { name: 'Нефть', value: oil },
-        { name: 'Жидкость', value: liq }
-    ]
+    const [data, setData] = useState<dataGOL[]>([]);
+
+
+    useEffect(() => {
+        const gas = props.dataMer.map(item => Number.parseInt(item["gas"].toFixed())).reduce((a, b) => a + b, 0);
+        const oil = props.dataMer.map(item => Number.parseInt(item["oil"].toFixed())).reduce((a, b) => a + b, 0);
+        const liq = props.dataMer.map(item => Number.parseInt(item["liq"].toFixed())).reduce((a, b) => a + b, 0);
+
+        setData([
+            { name: 'Газ, м3', value: gas },
+            { name: 'Нефть, т', value: oil },
+            { name: 'Жидкость, т', value: liq }
+        ]);
+    }, [props.dataMer]);
+
 
     const theme = useTheme();
 
+    const chartData = useMemo(() => data.slice(), [props.dataMer, data]);
     return (
         <>
             <Title>Общая добыча</Title>
             <ResponsiveContainer width="100%" height="100%">
                 <PieChart width={400} height={400}>
                     <Pie
-                        data={data}
+                        data={chartData}
                         cx="50%"
                         cy="50%"
                         labelLine={false}
-                        outerRadius={150}
+                        outerRadius={115}
                         fill="#8884d8"
                         dataKey="value"
+                        label
                     >
-                        {data.map((entry, index) => (
+                        {chartData.map((entry, index) => (
                             <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                         ))}
                     </Pie>
